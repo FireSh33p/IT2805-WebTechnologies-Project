b57c11223746037dd8001af0f88cb783b01075db
Snorre Sæther <snosaet@hotmail.com>
2022-10-20 12:59:46 +0200

    start navbar

---
+++ HTML/navbar.js
@@ -0,0 +1,9 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <nav>            
+         {*Header code goes here *}         
+        </nav>
+      `;
+    }
+  }
