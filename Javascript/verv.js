7ab9ce072912fa1ed0f1522514844b9f47e0f382
Arman Nemati <armanin@stud.ntnu.no>
2022-11-08 15:43:21 +0100

    endrer på kort

--- Javascript/verv.js
+++ Javascript/verv.js
@@ -6,10 +6,12 @@ function cardClick(event){
     const card_content = card.getElementsByTagName('div')[1];
     const card_category = card_content.getElementsByTagName('p')[0];
     const card_face2 = card_content.getElementsByTagName('div')[2];
+    const card_background = card.getElementsByTagName('div')[0];
     card_content.classList.toggle("card_content_open");
     card_content.classList.toggle("card_content_closed");
     card_category.classList.toggle('card_category_open');
     card_category.classList.toggle('card_category_closed');
     card_face2.classList.toggle('face2_open');
-    card_face2.classList.toggle('face2_closed');   
+    card_face2.classList.toggle('face2_closed');
+    card_background.style.filter = "brightness(0.5) saturate(0) contrast(1.2) blur(20px)";
 }
