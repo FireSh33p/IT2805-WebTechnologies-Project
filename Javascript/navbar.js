5927b87b5eb1dcaa48254cffad9370487d448b38
Snorre Sæther <snosaet@hotmail.com>
2022-11-10 00:42:44 +0100

    navbar helt ferdig

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -4,7 +4,21 @@ class Navbar extends HTMLElement {
       this.innerHTML = `
 
       <style>
-      .nav_ikon{
+      nav{
+        position: sticky;
+        position: -webkit-sticky;
+        top: 0;
+     
+        height: 100%; /*Denne gjør susen*/
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        z-index: 99;
+        
+        }
+    
+    .nav_ikon{
         position:absolute;
         top:0;
         right: 0;
@@ -15,19 +29,19 @@ class Navbar extends HTMLElement {
         border-radius: 5px;
     
         background-color: #634d8d;
-    
         background-image: url("../IMG/burgerIkon2.svg");
-    
         background-position: center;
         background-repeat: no-repeat;
         
         cursor: pointer;
-        z-index: 99;
     }
     .mobil_logo{
+        position: absolute;
         display: block;
+        top:0;
+        left:0;
     
-        margin:10px;
+        margin: 11px 0px 0px 10px;
         height: 70px;
         width: 70px;
     
@@ -35,32 +49,22 @@ class Navbar extends HTMLElement {
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-    
         cursor: pointer;
-        z-index: 99;
     }
-    nav{
-        position: sticky;
-        position: -webkit-sticky;
-        top: 0;
-        display: flex;
-        flex-direction: column;
-        z-index: 99;
-        }
     
     nav ul{
         display: none;
         list-style-type: none;
-        z-index: 99;
+        padding: 0;
         }
     
     nav ul li a{
         display: block;
-        min-width: 100px;
+        min-width: 150px;
         
-        margin: 10px;    
-        padding: 10px;
-        border-radius: 15px;
+        margin: 25px;    
+        padding: 20px;
+        border-radius: 35px;
     
         background-color: #634d8d;
     
@@ -79,10 +83,10 @@ class Navbar extends HTMLElement {
         }
         
     .nav_check:checked ~ ul{
-            display:flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+            position: absolute;
+            display: block;
+            margin-top: 100px;
+            width: 50vw;
         }
     
     .nav_check:checked + .nav_ikon{
@@ -102,8 +106,9 @@ class Navbar extends HTMLElement {
         }
     
         nav ul{
-            display: flex !important;
-            flex-direction: row !important;
+            position: absolute;
+            display: flex;
+            flex-direction: row;
             justify-content: center;
             align-items: center;
             margin:0;
@@ -130,42 +135,49 @@ class Navbar extends HTMLElement {
             background-position: center;
             background-repeat: no-repeat;
         } 
+    
     }
       </style>
 
 
-<!--##HTML-NAVBAR-START##-->
-
+      <!--##HTML-NAVBAR-START##-->
       <nav>
               <a class="mobil_logo" href="#" ></a>
               <input class="nav_check" type="checkbox" id="nav_check" />
               <label class="nav_ikon" for="nav_check"></label>
+
+      
              <ul>
              <!--Venstre navn linker-->
              <li>
-             <a href="../HTML/info.html">INFO</a>
+             <a href="https://www.nrk.no/">INFO</a>
              </li>
              <li>
-             <a href="../HTML/Verv.html">VERV</a>
+             <a href="#">VERV</a>
              </li>
              <!--Sentrert navbar logo-->
              <li>
-                     <a href="hovedside.html"  class="nav_senter" ></a>
+                     <a href="#"  class="nav_senter" ></a>
              </li>
              <!--Høyre  linker-->
              <li class="nav_challenges">
-             <a href="../HTML/challenges.html">CHALLENGES</a>   
+             <a href="#">CHALLENGES</a>   
              </li>
              <li class="nav_kontakOss">
-             <a href="../HTML/kontaktOss.html">KONTAKT OSS</a>
+             <a href="#">KONTAKT OSS</a>
              </li>
              </ul>
+      
              </nav>
 
 <!--##HTML-NAVBAR-START##-->
-
 
       `;
     }
   }
   customElements.define('nav-bar', Navbar);
+  window.addEventListener("resize", function() {
+    if (window.matchMedia("(min-width: 850px)").matches) {
+    document.getElementById("nav_check").checked = false;
+    } 
+    })
