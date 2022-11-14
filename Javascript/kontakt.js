3a4d6ec56f4227047704cfccc204bc643850c9d2
Daniel Gulbrandsen <danielgulbrandsen@dhcp-10-22-5-92.wlan.ntnu.no>
2022-11-14 15:08:38 +0100

    kontakt oss JS

---
+++ Javascript/kontakt.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("send_inn")
+
+
+function alert(){
+    alert("Takk for at du tar kontakt, vi vil kontakte deg så snart som mulig");
+}
+
+button.addEventListener("click",alert);
